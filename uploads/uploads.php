23d8dcfd85e0fa9c3ae6fb5260dbfe6de773dfde
cleanep <166646729+cleanep@users.noreply.github.com>
2024-05-06 11:28:58 +0200

    Add files via upload

---
+++ uploads/uploads.php
@@ -0,0 +1,18 @@
+﻿<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $file = $_FILES['file'];
+    $file_name = $file['name'];
+    $file_temp = $file['tmp_name'];
+    $file_size = $file['size'];
+    $file_error = $file['error'];
+
+    // Sprawdź rozmiar pliku
+    if ($file_size < 1000000) { // 1MB limit
+        $file_destination = 'uploads/' . $file_name;
+        move_uploaded_file($file_temp, $file_destination);
+        echo "Plik został przesłany pomyślnie.";
+    } else {
+        echo "Twój plik jest za duży!";
+    }
+}
+?>
