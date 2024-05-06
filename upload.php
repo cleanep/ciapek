3eb83c84017056078da553eda23d3334b524f14f
cleanep <166646729+cleanep@users.noreply.github.com>
2024-05-06 12:01:08 +0200

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,18 +1,31 @@
-﻿<?php
+<?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $file = $_FILES['file'];
-    $file_name = $file['name'];
-    $file_temp = $file['tmp_name'];
-    $file_size = $file['size'];
-    $file_error = $file['error'];
+    $uploadDir = 'uploads/';
+    $uploadedFiles = [];
+    foreach ($_FILES['file']['tmp_name'] as $key => $tmp_name) {
+        $file_name = $_FILES['file']['name'][$key];
+        $file_tmp = $_FILES['file']['tmp_name'][$key];
+        $file_type = $_FILES['file']['type'][$key];
+        $file_size = $_FILES['file']['size'][$key];
+        $file_error = $_FILES['file']['error'][$key];
 
-    // Sprawdź rozmiar pliku
-    if ($file_size < 10000000) { // 10MB limit
-        $file_destination = 'uploads/' . $file_name;
-        move_uploaded_file($file_temp, $file_destination);
-        echo "Plik został przesłany pomyślnie.";
-    } else {
-        echo "Twój plik jest za duży!";
+        if ($file_error == UPLOAD_ERR_OK && is_uploaded_file($file_tmp)) {
+            if (!move_uploaded_file($file_tmp, $uploadDir . $file_name)) {
+                echo "Przepraszamy, wystąpił problem podczas przesyłania plików.";
+            } else {
+                $uploadedFiles[] = $file_name;
+            }
+        } else {
+            echo "Przepraszamy, wystąpił problem podczas przesyłania plików.";
+        }
+    }
+
+    // Wyświetlenie przesłanych plików
+    if (!empty($uploadedFiles)) {
+        echo "Przesłano następujące pliki:<br>";
+        foreach ($uploadedFiles as $file) {
+            echo $file . "<br>";
+        }
     }
 }
 ?>
