55e8a39cc1c7bfcc3c97390e7ec071d10c005f35
cleanep <166646729+cleanep@users.noreply.github.com>
2024-05-06 15:57:25 +0200

    Update upload.php

--- uploads/upload.php
+++ uploads/upload.php
@@ -1,18 +1,1 @@
-﻿<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $file = $_FILES['file'];
-    $file_name = $file['name'];
-    $file_temp = $file['tmp_name'];
-    $file_size = $file['size'];
-    $file_error = $file['error'];
 
-    // Sprawdź rozmiar pliku
-    if ($file_size < 10000000) { // 10MB limit
-        $file_destination = 'uploads/' . $file_name;
-        move_uploaded_file($file_temp, $file_destination);
-        echo "Plik został przesłany pomyślnie.";
-    } else {
-        echo "Twój plik jest za duży!";
-    }
-}
-?>
